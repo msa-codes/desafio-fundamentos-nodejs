5d561d624b3c4ead03707d2bb957ad289a89a6cc
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2020-07-06 09:17:01 -0300

    Desafio 05 - Primeiro projeto NodeJS

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { transactions } = this;
+
+    const { income, outcome } = transactions.reduce(
+      (acc: Balance, ele: Transaction): Balance => {
+        if (ele.type === 'income') {
+          acc.income += ele.value;
+        }
+        if (ele.type === 'outcome') {
+          acc.outcome += ele.value;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
